89b5d3f04c88519a3ab530225c3dd5ba8a5a4302
Aparesh <kraparesh@gmail.com>
2023-08-01 22:15:59 +0530

    Add: Spring Batch Processor

--- src/main/java/io/kraparesh/ipldashboard/data/MatchDataProcessor.java
+++ src/main/java/io/kraparesh/ipldashboard/data/MatchDataProcessor.java
@@ -1,24 +1,55 @@
 package io.kraparesh.ipldashboard.data;
 
+import java.time.LocalDate;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.batch.item.ItemProcessor;
 
-public class MatchDataProcessor implements ItemProcessor<MatchDataProcessor, Person> {
+import io.kraparesh.model.Match;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
 
   private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
 
   @Override
-  public Person process(final Person person) throws Exception {
-    final String firstName = person.getFirstName().toUpperCase();
-    final String lastName = person.getLastName().toUpperCase();
+  public Match process(final MatchInput matchInput) throws Exception {
+    
+    Match match = new Match();
+    match.setId(Long.parseLong(matchInput.getId()));
+    match.setCity(matchInput.getCity());
+    
+    match.setDate(LocalDate.parse(matchInput.getDate()));
 
-    final Person transformedPerson = new Person(firstName, lastName);
+    match.setPlayerOfMatch(matchInput.getPlayer_of_match());
 
-    log.info("Converting (" + person + ") into (" + transformedPerson + ")");
+    match.setVenue(matchInput.getVenue());
 
-    return transformedPerson;
+    //Set team 1 and Team 2 depending on the innings order
+    String firstInningsTeam, secondInningsTeam;
+
+    if("bat".equals(matchInput.getToss_decision())) {
+      firstInningsTeam = matchInput.getToss_winner();
+      secondInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) ?
+                          matchInput.getTeam1() : matchInput.getTeam2();
+    } else {
+      secondInningsTeam = matchInput.getToss_winner();
+      firstInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1()) ?
+                         matchInput.getTeam2() : matchInput.getTeam1();
+    }
+
+    match.setTeam1(firstInningsTeam);
+    match.setTeam2(secondInningsTeam);
+
+    match.setTossWinner(matchInput.getToss_winner());
+    match.setTossDecision(matchInput.getToss_decision());
+    match.setResult(matchInput.getResult());
+    match.setResultMargin(match.getResultMargin());
+    match.setUmpire1(matchInput.getUmpire1());
+    match.setUmpire2(matchInput.getUmpire2());
+
+    return match;
   }
 
 }
