42c39a4f13cf5968c447e7391cd20bad446da332
Aparesh <kraparesh@gmail.com>
2023-08-16 21:39:01 +0530

    Add: Get matches by year api

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
 
 
 
@@ -13,6 +14,7 @@ function App() {
       <Router>
         <Routes>
             <Route path="/team/:teamName" Component={TeamPage} />
+            <Route path="/team/:teamName/matches/:year" Component={MatchPage} />
         </Routes>
       </Router>
 
